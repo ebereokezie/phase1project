c37419f9a6e3bc7dc9ea670dbcc94a3a183202af
Ebere Okezie <ebereokezie@gmail.com>
2022-06-04 13:09:51 -0500

    add card information

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {let clean_data = data["artists"][0]
     renderData(clean_data)})
 
+    function renderData(element) {
 
+        let albumCard = document.createElement("div")
+        albumCard.classList.add("album-card")
+        let name = document.createElement("h2")
+        name.textContent = `${element.name}`
+        let picture = document.createElement("img")
+        picture.classList.add("album-picture")
+        picture.src= "https://static4.depositphotos.com/1026550/376/i/600/depositphotos_3763236-stock-photo-gold-star.jpg"
+        let artistType = document.createElement("p")
+        let countryOfOrigin = document.createElement("p")
+        artistType.textContent = `Artist type: ${element.disambiguation}`
+        countryOfOrigin.textContent= `Country of Origin: ${element.country}`
+        let favoriteButton = document.createElement("button")
+        favoriteButton.classList.add("button")
+        favoriteButton.textContent = "Favorite"
+        albumCard.appendChild(name)
+        albumCard.appendChild(picture)
+        albumCard.appendChild(artistType)
+        albumCard.appendChild(countryOfOrigin)
+        albumCard.appendChild(favoriteButton)
+        albumContainer.appendChild(albumCard)
+    
+    }
+    let albumContainer = document.getElementById("album-container")
 
 
 
