d8bb4303d50fb08431eef08c230b1b738abd6ed4
Ebere Okezie <ebereokezie@gmail.com>
2022-06-04 20:38:00 -0500

    add submit button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch(`https://musicbrainz.org/ws/2/artist/?fmt=json&query=name:nirvana`)
+    fetch(`https://musicbrainz.org/ws/2/artist/?fmt=json&query=name:snoop dogg`)
     .then(data => data.json())
     .then(data => {let clean_data = data["artists"][0]
     renderData(clean_data)})
